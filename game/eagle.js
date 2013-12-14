1b938443bbe17e1d787656f62c57a7c4fb00f654
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2013-12-14 20:43:48 +0100

    You can go sideways.

--- game/eagle.js
+++ game/eagle.js
@@ -1,19 +1,28 @@
 function Eagle(game) {
   Entity3.call(this);
   this.keyboard = Keyboard.getInstance();
+  this.speed = 0.001;
 }
 
 Eagle.prototype.update = function(timeDelta) {
   if(this.keyboard.keysPressed.space) {
-    this.transformation.translateY(0.001*timeDelta);
-  } else {
-    this.transformation.translateY(-0.0001*timeDelta);
+    this.transformation.translateY(this.speed*timeDelta);
   }
+  this.transformation.translateY(-0.0001*timeDelta);
 
+  var translation, forward;
+  if(this.keyboard.keysPressed.w || this.keyboard.keysPressed.s || this.keyboard.keysPressed.a || this.keyboard.keysPressed.d) {
+    forward = Direction3D.transform(Direction3D.forward(), this.transformation.getWorldMatrix());
+  }
   if(this.keyboard.keysPressed.w || this.keyboard.keysPressed.s) {
-    var forward = Direction3D.transform(Direction3D.forward(), this.transformation.getWorldMatrix());
-    var translation = Vector3.multiply(forward, timeDelta*0.001);
+    translation = Vector3.multiply(forward, timeDelta*this.speed);
     if(this.keyboard.keysPressed.s) translation.negate();
+    this.transformation.translate(translation);
+  }
+  if(this.keyboard.keysPressed.a || this.keyboard.keysPressed.d) {
+    var left = Direction3D.cross(forward, Direction3D.up());
+    translation = Vector3.multiply(left, timeDelta*this.speed);
+    if(this.keyboard.keysPressed.d) translation.negate();
     this.transformation.translate(translation);
   }
 
--- game/world_scene.js
+++ game/world_scene.js
@@ -12,12 +12,12 @@ WorldScene.prototype = Object.create(Scene.prototype);
 
 WorldScene.prototype.setupLight = function() {
   var directionalLight1 = new DirectionalLight();
-  directionalLight1.direction = Vector3.normalize(new Vector3(-0.3, -1, 0));
+  directionalLight1.direction = Vector3.normalize(new Vector3(-0.5, -1, 0));
   directionalLight1.intensity = new Color(0.3, 0.3, 0.3);
   this.game.renderer.setDirectionalLight(directionalLight1, 1);
 
   var directionalLight2 = new DirectionalLight();
-  directionalLight2.direction = Vector3.normalize(new Vector3(-0.3, 1, 0));
+  directionalLight2.direction = Vector3.normalize(new Vector3(0.5, 1, 0));
   directionalLight2.intensity = new Color(0.1, 0.1, 0.1);
   this.game.renderer.setDirectionalLight(directionalLight2, 2);
 
