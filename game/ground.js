49b7d32954689386ba82269544ecd9331846b55f
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2013-12-15 20:36:45 +0100

    Tuned ground color.

--- game/ground.js
+++ game/ground.js
@@ -1,7 +1,7 @@
 function Ground(game) {
   Entity3.call(this);
   var size = new Vector2(200, 200);
-  var planeMesh = new PlaneMesh(Point3D.origin(), size, Direction3D.forward(), Direction3D.up(), new Color(0.1, 0.9, 0.3));
+  var planeMesh = new PlaneMesh(Point3D.origin(), size, Direction3D.forward(), Direction3D.up(), new Color(0.52, 0.91, 0.32));
   this.transformation.setPositionZ(1);
   var rendering = new MeshRendering(this, planeMesh, { static: true });
   game.renderer.addMeshRendering(rendering);
