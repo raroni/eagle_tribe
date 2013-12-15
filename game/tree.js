0b2a785f17d67efa1ae748be6df3c451d7cc4952
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2013-12-15 17:52:25 +0100

    Random tree y rotation.

--- game/tree.js
+++ game/tree.js
@@ -2,6 +2,7 @@ function Tree(game, position) {
   Entity3.call(this);
 
   this.transformation.setPosition(position);
+  this.transformation.rotateY(Math.random()*Math.PI*2);
 
   var trunkRendering = new MeshRendering(this, game.meshes.get('tree'), { static: true });
 
